5703acd45481570b5ea0fa44736600bf14bfdbf4
sergereinov <47950629+sergereinov@users.noreply.github.com>
2022-05-23 18:02:14 +0300

    Update cache.go

--- cache.go
+++ cache.go
@@ -2,22 +2,42 @@ package cache
 
 import "time"
 
+type record struct {
+	value string
+	deadline time.Time
+}
+
 type Cache struct {
+	cache map[string]record
 }
 
 func NewCache() Cache {
-	return Cache{}
+	return Cache{make(map[string]record, 0)}
 }
 
-func (receiver) Get(key string) (string, bool) {
-
+func (c *Cache) Get(key string) (string, bool) {
+	s, ok := c.cache[key]
+	if ok && (s.deadline.IsZero() || s.deadline.After(time.Now())) {
+		return s.value, ok
+	}
+	return "", false
 }
 
-func (receiver) Put(key, value string) {
+func (c *Cache) Put(key, value string) {
+	c.cache[key] = record{value, time.Time{}}
 }
 
-func (receiver) Keys() []string {
+func (c *Cache) Keys() []string {
+	keys := make([]string, 0, len(c.cache))
+	for k, v := range(c.cache) {
+		if v.deadline.IsZero() || v.deadline.After(time.Now()) {
+			keys = append(keys, k)
+		}		
+	}
+
+	return keys
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (c *Cache) PutTill(key, value string, deadline time.Time) {
+	c.cache[key] = record{value, deadline}
 }
